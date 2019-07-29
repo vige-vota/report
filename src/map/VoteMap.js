ac476cedd63f59e8a792722225adc045fbd2b2cc
flashboss <luca.stancapiano@vige.it>
2019-07-29 18:22:18 +0200

    wheel on maps

--- src/map/VoteMap.js
+++ src/map/VoteMap.js
@@ -8,6 +8,8 @@ import {
 } from 'react-simple-maps'
 import './VoteMap.css'
 import { Button } from 'primereact/button'
+import { AutoComplete } from 'primereact/autocomplete'
+import { FormattedMessage } from 'react-intl'
 
 const colorScale = scaleLinear()
 .domain([0, 100000000, 13386129701]) // Max is based on China
@@ -20,12 +22,22 @@ class VoteMap extends Component {
 
 		this.state = {
 		   zoom: 3,
+		   siteSuggestions: null
 		}
+		this.sites = ['Audi', 'BMW', 'Fiat', 'Ford', 'Honda', 'Jaguar', 'Mercedes', 'Renault', 'Volvo'];
 
 		this.handleZoomIn = this.handleZoomIn.bind(this)
 		this.handleZoomOut = this.handleZoomOut.bind(this)
 		this.handleRefresh = this.handleRefresh.bind(this)
 		this.handleWheel = this.handleWheel.bind(this)
+	}
+
+	suggestSites(event) {
+	    let results = this.sites.filter((site) => {
+	         return site.toLowerCase().startsWith(event.query.toLowerCase());
+	    });
+	    
+	    this.setState({ siteSuggestions: results });
 	}
 		  
 	handleRefresh() {
@@ -65,7 +77,16 @@ class VoteMap extends Component {
 		let topoMap = require('./topo.json')
 		return(
 			<div>
-		        <ComposableMap>
+				<FormattedMessage
+            	id='app.search'
+            	defaultMessage='Search site...'>
+					{(placeholder) => <AutoComplete className='searchsites' value={this.state.site} onChange={(e) => this.setState({site: e.value})}
+						placeholder={placeholder} 
+						suggestions={this.state.siteSuggestions} 
+						completeMethod={this.suggestSites.bind(this)} size={38} /> }
+		    	</FormattedMessage>
+				<Button id='btnSearch' icon='pi pi-search' />
+				<ComposableMap>
 		          	<ZoomableGroup zoom={ this.state.zoom }>
 		          		<Geographies geography={ topoMap }>
 		          		{(geographies, projection) => geographies.map(geography => (
@@ -93,11 +114,34 @@ class VoteMap extends Component {
 		          		</Geographies>
 		          	</ZoomableGroup>
 		        </ComposableMap>
-		        <div id='bottoni-zoom'>
-	        		<Button id='btnRefresh' icon='pi pi-refresh' className='fa fa-3x' onClick={ this.handleRefresh } />
-		        	<Button id='btnZoomIn' icon='pi pi-plus' className='fa fa-3x' onClick={ this.handleZoomIn } />
-		        	<Button id='btnZoomOut' icon='pi pi-minus' className='fa fa-3x' onClick={ this.handleZoomOut } />
+		        <div id='buttons-zoom'>
+	        		<Button id='#btnResetZoom' icon='pi pi-refresh' onClick={ this.handleRefresh } />
+		        	<Button id='btnZoomIn' icon='pi pi-plus' onClick={ this.handleZoomIn } />
+		        	<Button id='btnZoomOut' icon='pi pi-minus' onClick={ this.handleZoomOut } />
 		        </div>
+	        	<div id='select-zoom'>
+					<FormattedMessage id='app.chooseregion'
+						defaultMessage='Choose region...'>
+						{(placeholder) => <AutoComplete className='chooseregion' value={this.state.site} onChange={(e) => this.setState({site: e.value})}
+						placeholder={placeholder} 
+						suggestions={this.state.siteSuggestions} 
+						completeMethod={this.suggestSites.bind(this)} size={19} /> }
+					</FormattedMessage>
+					<FormattedMessage id='app.chooseprovince'
+						defaultMessage='Choose province...'>
+						{(placeholder) => <AutoComplete className='chooseprovince' value={this.state.site} onChange={(e) => this.setState({site: e.value})}
+						placeholder={placeholder} 
+						suggestions={this.state.siteSuggestions} 
+						completeMethod={this.suggestSites.bind(this)} size={19} /> }
+					</FormattedMessage>
+					<FormattedMessage id='app.choosecity'
+						defaultMessage='Choose city...'>
+						{(placeholder) => <AutoComplete className='choosecity' value={this.state.site} onChange={(e) => this.setState({site: e.value})}
+						placeholder={placeholder} 
+						suggestions={this.state.siteSuggestions} 
+						completeMethod={this.suggestSites.bind(this)} size={42} /> }
+					</FormattedMessage>
+	        	</div>
 		    </div>
 		)
 	}
