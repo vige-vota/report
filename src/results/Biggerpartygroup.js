83862ca3ad8251bf8b7f245fa44d3cd22cea323d
Luca Stancapiano <lucastancapiano@MacBook-Pro.local>
2023-05-19 12:05:01 +0200

    graphical updates

--- src/results/Biggerpartygroup.js
+++ src/results/Biggerpartygroup.js
@@ -100,7 +100,7 @@ export class Biggerpartygroup extends Component {
             if (this.props.app.state.activeTabVote.id === 0)
             	dataTable = <DataTable value={value} sortField='votes' sortOrder={-1} 
             			 footerColumnGroup={footer} className='biggernogroup-sub-header'>
-            				<Column />
+            				<Column style={{width: '8.4%' }} />
             				<Column field='image' body={this.partyTemplate} style={{width:'10%'}} />
         					<Column field='name' style={{width: '70%' }} body={this.candidatesTemplate} />
         					<Column />
